bdf10b971ab876a4ef9064e11e263657f84404a4
squadoCoding <106444206+squadoCoding@users.noreply.github.com>
2022-11-28 20:53:52 +0530

    new changes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const { json } = require("express");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const multer = require("multer");
+// I want to uninstall multer later
+// const multer = require("multer");
+// const upload = multer({ dest: "pdfs/" });
 const mergePDFs = require("./mergePDF");
 
 const app = express();
-const upload = multer({ dest: "pdfs/" });
 
 let myDate = new Date();
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./static"));
 app.use(express.static("./pdfs"));
 
 const port = 8000;
@@ -18,15 +23,16 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/merge", upload.array("pdfs", 10), (req, res, next) => {
-  uniqueName = myDate.getTime();
-  req.files.forEach(async (file, index) => {
-    let fileName = await mergePDFs("/pdfs/" + file.filename);
-    if (index == req.files.length - 1) {
-      res.redirect("/" + fileName + ".pdf");
-    }
-  });
-});
+// app.post("/merge", pdfs.array("pdfs", 10), (req, res, next) => {
+//   console.log(req);
+//   uniqueName = myDate.getTime();
+//   req.files.forEach(async (file, index) => {
+//     let fileName = await mergePDFs("/pdfs/" + file.filename);
+//     if (index == req.files.length - 1) {
+//       res.redirect("/" + fileName + ".pdf");
+//     }
+//   });
+// });
 
 app.listen(port, () => {
   console.log("Listening on http://localhost:8000");
